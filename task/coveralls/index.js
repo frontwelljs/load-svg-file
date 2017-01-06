9d135e4e1921dac3ca8d1a8c42427231b035097a
Richard Szakacs <richardszkcs@gmail.com>
2017-01-06 06:29:14 +0100

    Add merging functionality of the multiple coverage reports

--- task/coveralls/index.js
+++ task/coveralls/index.js
@@ -2,10 +2,18 @@
 
 const gulp = require('gulp')
 const debug = require('gulp-debug')
+const tap = require('gulp-tap')
+const merger = require ('lcov-result-merger')
 const coveralls = require('gulp-coveralls')
 
 gulp.task('coveralls', () => {
   return gulp
     .src('../coverage/**/lcov.info')
+    .pipe(debug())
+    .pipe(merger())
+    //.pipe(debug())
+    .pipe(tap(function(file) {
+      console.log(file.contents.toString())
+    }))
     .pipe(coveralls())
 })
