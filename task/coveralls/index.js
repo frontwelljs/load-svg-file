98ca200ee3eb883318889d86925ab1d9acec88b0
Richard Szakacs <richardszkcs@gmail.com>
2017-01-06 06:37:32 +0100

    Tinker with code coverage merging

--- task/coveralls/index.js
+++ task/coveralls/index.js
@@ -3,6 +3,7 @@
 const gulp = require('gulp')
 const debug = require('gulp-debug')
 const tap = require('gulp-tap')
+const concat = require('gulp-concat')
 const merger = require ('lcov-result-merger')
 const coveralls = require('gulp-coveralls')
 
@@ -10,8 +11,10 @@ gulp.task('coveralls', () => {
   return gulp
     .src('../coverage/**/lcov.info')
     .pipe(debug())
-    .pipe(merger())
+    //.pipe(merger())
     //.pipe(debug())
+    .pipe(concat('lcov.info'))
+    .pipe(debug())
     .pipe(tap(function(file) {
       console.log(file.contents.toString())
     }))
