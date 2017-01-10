2bc1c040925dfd9cc5cecd9a3413b041800b4a2a
Richard Szakacs <richardszkcs@gmail.com>
2017-01-10 07:08:23 +0100

    Tinker with coveralls.io

--- task/coveralls/index.js
+++ task/coveralls/index.js
@@ -1,21 +1,10 @@
 'use strict'
 
 const gulp = require('gulp')
-const debug = require('gulp-debug')
-const tap = require('gulp-tap')
 const coveralls = require('gulp-coveralls')
 
 gulp.task('coveralls', () => {
-  const pkg = require('../../package')
-
   return gulp
     .src('../coverage/lcov.info')
-    .pipe(tap(file => {
-      let content = file.contents.toString()
-
-      content = content.replace(/(SF):(.*index.js)/, (matches, prefix) => `${prefix}:${pkg.name}.js`)
-
-      file.contents = Buffer.from(content)
-    }))
     .pipe(coveralls())
 })
