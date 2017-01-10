96f89d409e45b754596eee382aeeb72f20fe2873
Richard Szakacs <richardszkcs@gmail.com>
2017-01-10 06:32:19 +0100

    Remove unneeded packages

--- task/coveralls/index.js
+++ task/coveralls/index.js
@@ -3,17 +3,11 @@
 const gulp = require('gulp')
 const debug = require('gulp-debug')
 const tap = require('gulp-tap')
-const concat = require('gulp-concat')
-const merger = require ('lcov-result-merger')
 const coveralls = require('gulp-coveralls')
 
 gulp.task('coveralls', () => {
   return gulp
-    .src('../coverage/**/lcov.info')
-    .pipe(debug())
-    //.pipe(merger())
-    //.pipe(debug())
-    .pipe(concat('lcov.info'))
+    .src('../coverage/lcov.info')
     .pipe(debug())
     .pipe(tap(function(file) {
       console.log(file.contents.toString())
