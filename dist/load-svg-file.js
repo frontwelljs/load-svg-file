cc77879430c7bf4ed171bc39db27ff1241076568
Richard Szakacs <richardszkcs@gmail.com>
2018-06-14 18:15:43 +0200

    Release v1.0.8

--- dist/load-svg-file.js
+++ dist/load-svg-file.js
@@ -1,6 +1,6 @@
 /**
  * @file Load SVG files over XHR and embed the SVG content into the DOM.
- * @version 1.0.7
+ * @version 1.0.8
  * @copyright Richard Szakacs, richardszkcs@gmail.com, www.richardszkcs.com
  * @license MIT
 */
