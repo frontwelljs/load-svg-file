7712aa5759a76a5ec2051907f22ee5a5bd3d6f10
Richard Szakacs <richardszkcs@gmail.com>
2018-06-14 16:15:06 +0200

    Update versioning

--- task/versioning/index.js
+++ task/versioning/index.js
@@ -7,11 +7,16 @@ gulp.task('versioning', done => {
   const pkg = require('../../package')
   const version = pkg.version
 
-  shell
-    .cd('..')
-    .exec('git add .')
-    .exec(`git commit -m 'Release v${version}'`)
-    .exec(`git tag v${version}`)
+  shell.cd('..')
+  shell.exec('git add .')
+  shell.exec(`git commit -m 'Release v${version}'`)
+  shell.exec(`git tag v${version}`)
+
+  const error = shell.error()
+
+  if (error) {
+    console.log(error)
+  }
 
   done()
 })
